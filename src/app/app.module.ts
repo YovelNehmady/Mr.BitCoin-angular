753e8a6a0e7b01787a8cd9bcfe2ad212a83484d1
YovelNehmady <yovelnehmady1998@gmail.com>
2023-02-19 18:55:45 +0200

    create new angular proj and added cmps and pages templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app-root/app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactDetailsComponent } from './pages/contact-details/contact-details.component';
+import { StatisticComponent } from './pages/statistic/statistic.component';
+import { ContactIndexComponent } from './pages/contact-index/contact-index.component';
+import { ContactListComponent } from './cmps/contact-list/contact-list.component';
+import { ContactPreviewComponent } from './cmps/contact-preview/contact-preview.component';
+import { ContactFilterComponent } from './cmps/contact-filter/contact-filter.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactDetailsComponent,
+    StatisticComponent,
+    ContactIndexComponent,
+    ContactListComponent,
+    ContactPreviewComponent,
+    ContactFilterComponent
   ],
   imports: [
     BrowserModule,
