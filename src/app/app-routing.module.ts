d7946a8e05db325cc24b8906e1fb0266f6a91c28
YovelNehmady <yovelnehmady1998@gmail.com>
2023-02-20 16:13:48 +0200

    added navigation & app header & routering

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { ContactIndexComponent } from './pages/contact-index/contact-index.component'
+import { HomeComponent } from './pages/home/home.component'
+import { StatisticComponent } from './pages/statistic/statistic.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '' , component : HomeComponent},
+{path: 'contact' , component : ContactIndexComponent},
+{path: 'statistic' , component : StatisticComponent},
+
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app-root/app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ContactDetailsComponent } from './pages/contact-details/contact-details.component';
-import { StatisticComponent } from './pages/statistic/statistic.component';
-import { ContactIndexComponent } from './pages/contact-index/contact-index.component';
-import { ContactListComponent } from './cmps/contact-list/contact-list.component';
-import { ContactPreviewComponent } from './cmps/contact-preview/contact-preview.component';
-import { ContactFilterComponent } from './cmps/contact-filter/contact-filter.component'
+import { AppComponent } from './app-root/app.component'
+import { HomeComponent } from './pages/home/home.component'
+import { ContactDetailsComponent } from './pages/contact-details/contact-details.component'
+import { StatisticComponent } from './pages/statistic/statistic.component'
+import { ContactIndexComponent } from './pages/contact-index/contact-index.component'
+import { ContactListComponent } from './cmps/contact-list/contact-list.component'
+import { ContactPreviewComponent } from './cmps/contact-preview/contact-preview.component'
+import { ContactFilterComponent } from './cmps/contact-filter/contact-filter.component';
+import { AppHeaderComponent } from './cmps/app-header/app-header.component'
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ContactFilterComponent } from './cmps/contact-filter/contact-filter.com
     ContactIndexComponent,
     ContactListComponent,
     ContactPreviewComponent,
-    ContactFilterComponent
+    ContactFilterComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
